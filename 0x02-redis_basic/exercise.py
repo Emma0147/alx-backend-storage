20a5f3c7a3e88915b3c8b66529da99fd99e5fc7a
Emma0147 <mromotoriogunzabere@gmail.com>
2023-07-19 13:00:15 +0000

    Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Redis basic.
+"""
+from typing import Union, Callable, Optional
+import redis
+import uuid
+
+
+class Cache:
+    """Cache class to handle redis operations."""
+    def __init__(self):
+        """stores an instance of the Redis client."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Takes and stores a data argument and returns a string."""
+        key = str(uuid.uuid4())
+        self._redis.mset({key: data})
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> str:
+        """
+        Takes a key string argument and an optional.
+        Callable argument named fn. This callable will be used to
+        convertthe data back to a desired format.
+        """
+        data = self._redis.get(key)
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, data: str) -> str:
+        """Returns str value of decoded byte """
+        return data.decode('utf-8')
+
+    def get_int(self, data: str) -> str:
+        """Returns int value of decoded byte """
+        return int(data)
